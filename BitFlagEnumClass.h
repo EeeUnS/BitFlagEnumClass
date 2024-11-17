c9185ff49a284b8421bea9b0cd068d1cc97773e6
EeeUnS <euns312510@gmail.com>
2024-11-17 11:40:37 +0900

    Create BitFlagEnumClass.h

---
+++ BitFlagEnumClass.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#define cast_num  static_cast<unsigned long long>
+#define cast_enum  static_cast<E>
+
+template <typename E>
+class BitFlagEnumClass
+{
+
+public:
+	E m_eFlag;
+
+	BitFlagEnumClass() {
+		m_eFlag = cast_enum(0);
+	}
+
+	BitFlagEnumClass(E flag) {
+		m_eFlag = flag;
+	}
+
+	BitFlagEnumClass operator=(E flag) {
+		m_eFlag = flag;
+		return *this;
+	}
+
+	BitFlagEnumClass operator=(BitFlagEnumClass flag) {
+		m_eFlag = flag.m_eFlag;
+		return *this;
+	}
+
+	BitFlagEnumClass operator|=(E flag) {
+		m_eFlag = cast_enum(cast_num(flag) | cast_num(m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator|=(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) | cast_num(flag.m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator&=(E flag) {
+		m_eFlag = cast_enum(cast_num(m_eFlag) & cast_num(flag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator&=(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) & cast_num(flag.m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator^=(E flag) {
+		m_eFlag = cast_enum(cast_num(m_eFlag) ^ cast_num(flag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator^=(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) ^ cast_num(flag.m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator|(E flag) {
+		m_eFlag = cast_enum(cast_num(m_eFlag) | cast_num(flag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator|(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) | cast_num(flag.m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator&(E flag) {
+		m_eFlag = cast_enum(cast_num(m_eFlag) & cast_num(flag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator&(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) & cast_num(flag.m_eFlag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator^(E flag) {
+		m_eFlag = cast_enum(cast_num(m_eFlag) ^ cast_num(flag));
+		return *this;
+	}
+
+	BitFlagEnumClass operator^(BitFlagEnumClass flag) {
+		m_eFlag = cast_enum(cast_num(flag.m_eFlag) ^ cast_num(flag.m_eFlag));
+		return *this;
+	}
+};
+
+
+template <typename E>
+BitFlagEnumClass<E> operator& (E flag, BitFlagEnumClass<E> FlagClass)
+{
+	return cast_enum(cast_num(flag) & cast_num(FlagClass.m_eFlag));
+}
+
+template <typename E>
+BitFlagEnumClass<E> operator| (E flag, BitFlagEnumClass<E> FlagClass)
+{
+	return cast_enum(cast_num(flag) | cast_num(FlagClass.m_eFlag));
+}
+
+template <typename E>
+BitFlagEnumClass<E> operator^ (E flag, BitFlagEnumClass<E> FlagClass)
+{
+	return cast_enum(cast_num(flag) ^ cast_num(FlagClass.m_eFlag));
+}
+
+#undef cast_int
+#undef cast_enum
